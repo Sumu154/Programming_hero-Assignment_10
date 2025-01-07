5a559680c56d81b9440a89249364deee5d38f3e5
Sumu154 <sumaiyatasnim5548@gmail.com>
2025-01-08 00:08:59 +0600

    imported all icons and image forr home section

--- CLient side frontend/src/components/HomeCompoents/HomeSection.jsx
+++ CLient side frontend/src/components/HomeCompoents/HomeSection.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import campaign from '../../assets/icons/campaign (1).png'
+import collect from '../../assets/icons/coin-collecting.png'
+import withdraw from '../../assets/icons/cash-withdrawal.png'
 
 const HomeSection = () => {
   return (
     <div className='w-[90%] mx-auto'>
-      <h3> Our featured Category </h3>
+      <h3> The mission of our organization. </h3>
+      <p> We provide a trusted platform for peoples of worldwide to support people and organizers. </p>
+      <div>
+        <div>
+          <div>
+            <img className='bg-yellow p-2 w-16 rounded-full ' src={campaign} alt="" />
+          </div>
+          
+        </div>
+      </div>
       
     </div>
   );

dc02420c3af14e0dd1174aab631d13c5b962874e
Sumu154 <sumaiyatasnim5548@gmail.com>
2025-01-07 23:29:38 +0600

    homepage improved with category section

--- CLient side frontend/src/components/HomeCompoents/HomeSection.jsx
+++ CLient side frontend/src/components/HomeCompoents/HomeSection.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const HomeSection = () => {
   return (
-    <div>
+    <div className='w-[90%] mx-auto'>
+      <h3> Our featured Category </h3>
       
     </div>
   );
